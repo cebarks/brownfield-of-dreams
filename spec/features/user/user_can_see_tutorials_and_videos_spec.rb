38acc102a643a3d336827b9ac70d4f0bba5c7a0e
Jimmy Smith <jaystansmith@gmail.com>
2019-02-04 13:34:33 -0700

    Fixed relationship on test.

--- spec/features/user/user_can_see_tutorials_and_videos_spec.rb
+++ spec/features/user/user_can_see_tutorials_and_videos_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 describe "As a logged in user" do
   before(:each) do
-    user = create(:user, id: 1, github_token: ENV["GITHUB_ACCESS_TOKEN"])
+    user = create(:user, github_token: ENV["GITHUB_ACCESS_TOKEN"])
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    tutorial = create(:tutorial, id: 1, title: "Back End Engineering - Prework")
-    video_1 = create(:video, video_id: 1, tutorial_id: 1, title: "Learn Ruby the Right Way", position: 1)
-    video_2 = create(:video, video_id: 2, tutorial_id: 1, title: "Learn Rails the Right Way", position: 2)
-    video_3 = create(:video, video_id: 3, tutorial_id: 1, title: "Learn Active Record Queries", position: 3)
-    user_vid_1 = create(:user_video, user_id: 1, video_id: 1)
-    user_vid_2 = create(:user_video, user_id: 1, video_id: 2)
-    user_vid_2 = create(:user_video, user_id: 1, video_id: 3)
+    tutorial = create(:tutorial, title: "Back End Engineering - Prework")
+    video_1 = create(:video, tutorial: tutorial, title: "Learn Ruby the Right Way", position: 1)
+    video_2 = create(:video, tutorial: tutorial, title: "Learn Rails the Right Way", position: 2)
+    video_3 = create(:video, tutorial: tutorial, title: "Learn Active Record Queries", position: 3)
+    user_vid_1 = create(:user_video, user: user, video: video_1)
+    user_vid_2 = create(:user_video, user: user, video: video_2)
+    user_vid_3 = create(:user_video, user: user, video: video_3)
   end
 
   describe "When I visit /dashboard" do
