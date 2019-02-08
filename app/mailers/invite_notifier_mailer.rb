38a047cd95121c521b56593eba78f07611cdb38c
Anten Skrabec <cebarks@gmail.com>
2019-02-07 19:44:03 -0700

    actually, it totally works, was an api issue

--- app/mailers/invite_notifier_mailer.rb
+++ app/mailers/invite_notifier_mailer.rb
@@ -3,7 +3,7 @@ class InviteNotifierMailer < ApplicationMailer
     @invite = InviteFacade.new(github_user, inviter)
 
     return nil unless @invite.invitee_email
-    
+
     mail(to: @invite.invitee_email, subject: "You've been invited by #{@invite.inviter_name}")
   end
 end
